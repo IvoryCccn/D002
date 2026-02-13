ec231b7f47a4a2eb96bb8993ef731a8553004bfa
我是不是有点胖了 <cn_553512@163.com>
2026-02-13 13:57:41 +0000

    without cancel standing order and change logic when publice success then private

--- isd_bot.py
+++ isd_bot.py
@@ -3,13 +3,13 @@ from fmclient import Agent, Market, Holding, Order, OrderSide, OrderType, Sessio
 
 # Trading account details
 FM_ACCOUNT = "fain-premium"
-FM_EMAIL = "nc681@d002"
-FM_PASSWORD = "nc681"
+FM_EMAIL = "trader11@d002"
+FM_PASSWORD = "LIPNE"
 FM_MARKETPLACE_ID = 1513
 PUBLIC_MARKET_ID = 2681
 PRIVATE_MARKET_ID = 2682
 
-NATURE_TRADER_ID = "T011"
+NATURE_TRADER_ID = "M000"
 
 # Enum for the roles of the bot
 class Role(Enum):
@@ -36,12 +36,10 @@ class IDSBot(Agent):
 
     _holdings: Holding | None
 
-    _private_order: Order | None
-    _public_buy_order: Order | None
-    _public_sell_order: Order | None
-    
+    _my_private_order: Order | None
+    _my_public_order: Order | None
+
     _waiting_for_server: bool
-    _my_standing_order: Order | None
 
     def __init__(self, account: str, email: str, password: str, marketplace_id: int, bot_type: BotType, bot_name: str = "FMBot"):
         super().__init__(account, email, password, marketplace_id, name=bot_name)
@@ -52,12 +50,10 @@ class IDSBot(Agent):
         self._bot_type = bot_type # store bot type
         self._holdings = None # store holding information for checking
 
-        self._private_order = None # store any order in private market
-        self._public_buy_order = None # store standing buy order in public market
-        self._public_sell_order = None # store standing sell order in public market
+        self._my_private_order = None # track my private market standing order
+        self._my_public_order = None # track my public market standing order
 
         self._waiting_for_server = False # check to avoid double order sending
-        self._my_standing_order = None # track my standing order
 
     def initialised(self):
         self._public_market = self.markets[PUBLIC_MARKET_ID]
@@ -71,7 +67,8 @@ class IDSBot(Agent):
             )
         
         self.inform(
-            f"Markets loaded: PUBLIC = {self._public_market.name} ({self._public_market.fm_id}), "
+            f"Markets loaded: "
+            f"PUBLIC = {self._public_market.name} ({self._public_market.fm_id}), "
             f"PRIVATE = {self._private_market.name} ({self._private_market.fm_id})"
         )
 
@@ -88,7 +85,16 @@ class IDSBot(Agent):
 
     def received_holdings(self, holdings: Holding):
         self._holdings = holdings
-        self.inform(f"Current holdings in my account:\n Cash: {holdings.cash / 100:.2f} ({holdings.cash_available / 100:.2f}), {", ".join([f"{market.name}: {asset.units} ({asset.units_available})" for market, asset in holdings.assets.items()])}")
+
+        lines = [f"Current holdings in my account:"]
+        lines.append(f"{'Account':<20} {'Total':>12} {'Available':>12}")
+        lines.append("-" * 46)
+        lines.append(f"{'Cash':<20} {holdings.cash / 100:>12.2f} {holdings.cash_available / 100:>12.2f}")
+
+        for market, asset in holdings.assets.items():
+            lines.append(f"{market.name:<20} {asset.units:>12} {asset.units_available:>12}")
+
+        self.inform("\n".join(lines))
 
     def received_orders(self, orders: list[Order]) -> None:
         best_private_buy: Order | None = None
@@ -103,8 +109,12 @@ class IDSBot(Agent):
             
             # check my order
             if order.mine:
-                self._my_standing_order = order
-                self.inform(f"I have a standing order: {order} in {order.market}")
+                if order.market.fm_id == PUBLIC_MARKET_ID:
+                    self._my_public_order = order
+                    self.inform(f"I have a standing order in PUBLIC market: {order}")
+                elif order.market.fm_id == PRIVATE_MARKET_ID:
+                    self._my_private_order = order
+                    self.inform(f"I have a standing order in PRIVATE market: {order}")
                 continue
             
             # judge market type
@@ -117,7 +127,6 @@ class IDSBot(Agent):
                     best_private_buy = order
                 elif is_public and (best_public_buy is None or order.price > best_public_buy.price):
                     best_public_buy = order
-                    self._public_buy_order = order
             
             # update best standing sell price (the lower the better)
             elif order.order_side is OrderSide.SELL:
@@ -125,92 +134,109 @@ class IDSBot(Agent):
                     best_private_sell = order
                 elif is_public and (best_public_sell is None or order.price < best_public_sell.price):
                     best_public_sell = order
-                    self._public_sell_order = order
 
         self.inform(
-            f"\n The best standing buy order in public market is [{best_public_buy.price if best_public_buy else None}]"
-            f"\n The best standing sell order in public market is [{best_public_sell.price if best_public_sell else None}]"
+            f"The best standing order in PUBLIC market: "
+            f"BUY order price is [{best_public_buy.price if best_public_buy else None}], "
+            f"SELL order price is [{best_public_sell.price if best_public_sell else None}]"
         )
-
+        
+        # identify private market signal
         private_signal: Order | None = best_private_buy if best_private_buy is not None else best_private_sell
+        margin = None
 
         if private_signal is None:
-            self._private_order = None
-            self.inform(f"No order in private market")
+            self._role = None
+            margin = None
+            self.inform(f"No order in PRIVATE market")
+            return
         else:
-            self._private_order = private_signal
-            self.inform(f"Order signal in private market is {private_signal.order_side} at price {private_signal.price}")
+            self.inform(f"Order signal in PRIVATE market is [{private_signal.order_side}] at price [{private_signal.price}]")
+
+            # update robot role if signal in private has changed
             new_role = Role.BUYER if private_signal.order_side == OrderSide.BUY else Role.SELLER
             if self._role != new_role:
                 self._role = new_role
-                self.inform(f"Role updated to: {self._role.name}")
+                self.inform(f"Robot role updated to [{self._role.name}]")
 
-        if private_signal is not None:
-            if new_role == Role.BUYER and best_public_sell is not None:
-                margin = self._calculate_margin(self._role, self._private_order, self._public_buy_order)
-            elif new_role == Role.SELLER and best_public_buy is not None:
-                margin = self._calculate_margin(self._role, self._private_order, self._public_sell_order)
+            # calculate profit margin
+            if new_role == Role.BUYER:
+                if best_public_sell is not None:
+                    margin = private_signal.price - best_public_sell.price
+                else:
+                    margin = None
+                    self.inform("no standing SELL order in PUBLIC market")
+            elif new_role == Role.SELLER:
+                if best_public_buy is not None:
+                    margin = best_public_buy.price - private_signal.price
+                else:
+                    margin = None
+                    self.inform("no standing BUY order in PUBLIC market")
             else:
                 margin = None
+                self.inform("cannot calculate margin due to missing best standing order price")
 
-        if self._waiting_for_server == False and margin is not None and margin > PROFIT_MARGIN:
-            self.inform(f"margin ({margin}) is bigger than target, take buying action to make profits")
-            if self._role == Role.BUYER:
-                # best ask
-                self._placing_order(OrderSide.BUY, self._public_market, self._public_sell_order.price)
-                self._placing_order(OrderSide.SELL, self._private_market, self._private_order.price)
-            elif self._role == Role.SELLER:
-                # best bid
-                self._placing_order(OrderSide.SELL, self._public_market, self._public_buy_order.price)
-                self._placing_order(OrderSide.BUY, self._private_market, self._private_order.price)
-        elif margin is not None and margin <= PROFIT_MARGIN:
-            self.inform(f"margin ({margin}) is not bigger than target, no action and wait")
+        # placing order if it is profitable
+        if margin != None:
+            if self._waiting_for_server == False and margin > PROFIT_MARGIN:
+                self.inform(f"margin ({margin}) is bigger than target, take buying action to make profits")
+                if self._role == Role.BUYER:
+                    if self._holdings.cash_available > best_public_sell.price and self._holdings.assets[self._private_market].units_available >= 1:
+                        # best ask
+                        self._placing_order(OrderSide.BUY, self._public_market, best_public_sell.price)
+                        self._placing_order(OrderSide.SELL, self._private_market, private_signal.price)
+                    else:
+                        self.inform("insufficient available cash or unit")
+                elif self._role == Role.SELLER:
+                    if self._holdings.cash_available > private_signal.price and self._holdings.assets[self._public_market].units_available >= 1:
+                        # best bid
+                        self._placing_order(OrderSide.SELL, self._public_market, best_public_buy.price)
+                        self._placing_order(OrderSide.BUY, self._private_market, private_signal.price)
+                    else:
+                        self.inform("insufficient available cash or unit")
+            elif margin <= PROFIT_MARGIN:
+                self.inform(f"margin ({margin}) is not bigger than target, no action and wait")
         else:
+            margin = None
             self.inform(f"margin is None, no action and wait")
 
     def order_accepted(self, order: Order) -> None:
-        self.inform(f"My order ({order}) was accepted in {order.market.name}. It received fm_id {order.fm_id} from Flex-E-Markets.")
+        self._waiting_for_server = False
 
-        # if I have a LIMIT order accepted, update my standing order tracking to include it's fm_id
-        if order.order_type is OrderType.LIMIT:
-            self._my_standing_order = order
+        self.inform(f"Order accepted in [{order.market.name}]: fm_id={order.fm_id}, side={order.order_side.name}, price={order.price}, traded={order.has_traded}")
 
-        # if I have a CANCEL order accepted, remove my standing order tracking
-        elif order.order_type is OrderType.CANCEL:
-            self._my_standing_order = None
+        # if order is not traded and not cancelled, this order is now a standing order
+        if order.order_type == OrderType.LIMIT and (not order.has_traded) and (not order.is_cancelled):
+            if order.market is not None:
+                if order.market.fm_id == PUBLIC_MARKET_ID:
+                    self._my_public_order = order
+                    
+                elif order.market.fm_id == PRIVATE_MARKET_ID:
+                    self._my_private_order = order
+        else:
+            # if order is traded or cancelled, this order is now not a standing order
+            if self._my_public_order is not None and order.fm_id == self._my_public_order.fm_id:
+                self._my_public_order = None
+            if self._my_private_order is not None and order.fm_id == self._my_private_order.fm_id:
+                self._my_private_order = None
 
     def order_rejected(self, info: dict[str, str], order: Order) -> None:
-        self.inform(f"My order ({order}) was rejected. Info: {info}")
+        self._waiting_for_server = False
+
+        self.warning(f"Order rejected in [{order.market.name}]: order={order} info={info}")
 
         # if I have a LIMIT order rejected, well it was never actually standing, so remove my standing order tracking
-        if order.order_type == OrderType.LIMIT:
-            self._my_standing_order = None
-
-    def _calculate_margin(self, role: Role, private_price: Order, public_price: Order) -> int:
-        """
-        Calculating the speculation margin in two markets in cents
-        If role is BUYER, margin = private price - best_public_sell
-        If role is SELLER, margin = best_public_buy - private_price
-        """
-        margin = None
-
-        if role == Role.BUYER:
-            margin =  private_price.price - public_price.price
-        elif role == Role.SELLER:
-            margin =  public_price.price - private_price.price
-
-        return margin
+        if self._my_public_order is not None and order.ref == self._my_public_order.ref:
+            self._my_public_order = None
+        if self._my_private_order is not None and order.ref == self._my_private_order.ref:
+            self._my_private_order = None
 
     def _placing_order(self, side: OrderSide, market: Market, price: int) -> None:
         """
         Send a 1-unit limit order into the at the given price
         """
-        if self._public_market is None:
-            self.warning("Cannot send order: public market not initialised.")
-            return
-        
-        if self._private_market is None:
-            self.warning("Cannot send order: private market not initialised.")
+        if market is None:
+            self.warning("Cannot send order: PUBLIC or PRIVATE market not initialised.")
             return
 
         new_order = Order.create_new(market)
@@ -224,7 +250,11 @@ class IDSBot(Agent):
 
         new_order.owner_or_target = NATURE_TRADER_ID if market.fm_id == PRIVATE_MARKET_ID else None
 
-        self._my_standing_order = new_order
+        if market.fm_id == PRIVATE_MARKET_ID:
+            self._my_private_order = new_order
+        elif market.fm_id == PUBLIC_MARKET_ID:
+            self._my_public_order = new_order
+
         self._waiting_for_server = True
 
         self.send_order(new_order)
--- note.py
+++ note.py
@@ -3,20 +3,6 @@
         Get the reasons of no trading
         """
         reasons: list[str] = []
-        if private_signal is None:
-            reasons.append("no private signal")
-            return reasons
-        
-        if self._role == Role.BUYER and best_public_sell is None:
-            reasons.append("no standing sell order in public market")
-        if self._role == Role.SELLER and best_public_buy is None:
-            reasons.append("no standing buy order in public market")
-        
-        if margin is None:
-            reasons.append("cannot calculate margin due to missing best standing order price")
-        else:
-            if margin < PROFIT_MARGIN:
-                reasons.append("profit margin too low")
         
         if self._holdings is None:
             reasons.append("no holding snapshot yet")
@@ -31,3 +17,10 @@
             reasons.append("already have a standing order in public market")
 
         return reasons
+
+
+        """
+        Calculating the speculation margin in two markets in cents
+        If role is BUYER, margin = private price - best_public_sell
+        If role is SELLER, margin = best_public_buy - private_price
+        """
